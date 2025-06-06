61e98f6e699130e77becabd5ff9a9d16eace1cbf
leedongyun01 <80802462+leedongyun01@users.noreply.github.com>
2025-06-06 23:51:05 +0900

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,6 +38,6 @@ io.on('connection', (socket) => {
 
 // 서버 실행
 const PORT = 5000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
